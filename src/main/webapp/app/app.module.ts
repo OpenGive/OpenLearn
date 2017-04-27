88a90fa943f8ec66a13a65cbc149c5340a9c316a
Levi <lfuller941@gmail.com>
2017-04-27 17:04:21 -0600

    Switched to use angular-cli workflow

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,54 +1,20 @@
-import './vendor.ts';
-
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Ng2Webstorage } from 'ng2-webstorage';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { OpengiveSharedModule, UserRouteAccessService } from './shared';
-import { OpengiveHomeModule } from './home/home.module';
-import { OpengiveAdminModule } from './admin/admin.module';
-import { OpengiveAccountModule } from './account/account.module';
-import { OpengiveEntityModule } from './entities/entity.module';
-
-import { LayoutRoutingModule } from './layouts';
-import { customHttpProvider } from './blocks/interceptor/http.provider';
-import { PaginationConfig } from './blocks/config/uib-pagination.config';
-
-import {
-    JhiMainComponent,
-    NavbarComponent,
-    FooterComponent,
-    ProfileService,
-    PageRibbonComponent,
-    ActiveMenuDirective,
-    ErrorComponent
-} from './layouts';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        LayoutRoutingModule,
-        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
-        OpengiveSharedModule,
-        OpengiveHomeModule,
-        OpengiveAdminModule,
-        OpengiveAccountModule,
-        OpengiveEntityModule
-    ],
-    declarations: [
-        JhiMainComponent,
-        NavbarComponent,
-        ErrorComponent,
-        PageRibbonComponent,
-        ActiveMenuDirective,
-        FooterComponent
-    ],
-    providers: [
-        ProfileService,
-        customHttpProvider(),
-        PaginationConfig,
-        UserRouteAccessService
-    ],
-    bootstrap: [ JhiMainComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class OpengiveAppModule {}
+export class AppModule { }
