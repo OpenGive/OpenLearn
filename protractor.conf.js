a1f069776a5e3849ecc15786b84141a82b9ee57b
Jeremy Leisy <jleisy@credera.com>
2017-05-16 14:27:31 -0600

    Fixed e2e tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,7 +6,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    'src/test/javascript/e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
@@ -21,7 +21,7 @@ exports.config = {
   },
   beforeLaunch: function() {
     require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
+      project: 'src/test/javascript/e2e/tsconfig.e2e.json'
     });
   },
   onPrepare() {
