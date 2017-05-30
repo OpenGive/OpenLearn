f43cc851505caed5a9588f7be2ba4a69087764fb
James McCreary <jmccreary@credera.com>
2017-05-30 14:40:04 -0500

    Added CRUD functionality for courses; Added delete functionality to HttpWrapperService (also in feature/program-management commits, should not result in merge conflicts)

--- src/main/webapp/app/services/course.service.ts
+++ src/main/webapp/app/services/course.service.ts
@@ -7,13 +7,42 @@ import {log} from "util";
 
 @Injectable()
 export class CourseService {
-  constructor(private _http: HttpWrapperService) {
-  }
-  getAllCourses(): Observable<Course[]> {
-    return this._http.get('/api/courses').map(resp => {
-      let json = resp.json();
-      return json as Course[];
-    });
-  }
+	constructor(private _http: HttpWrapperService) {}
+
+	getAllCourses(): Observable<Course[]> {
+		return this._http.get('/api/courses').map(resp => {
+			let json = resp.json();
+			return json as Course[];
+		});
+	}
+
+	createCourse(courseToCreate: Course) {
+		return this._http.post('/api/courses', courseToCreate)
+		.map(response => <Course> response.json())
+		.catch(this.handleError);
+	}
+
+	updateCourse(courseToUpdate: Course): Observable<Course> {
+		return this._http.put('/api/courses', courseToUpdate)
+		.map(response => <Course> response.json())
+		.catch(this.handleError);
+	}
+
+	deleteCourse(id: number) {
+		return this._http.delete('/api/courses/' + id)
+		.map(response => response.json())
+		.catch(this.handleError);
+	}
+
+	getCourse(id: number): Observable<Course> {
+		return this._http.get('/api/courses/' + id)
+		.map(response => <Course> response.json())
+		.catch(this.handleError)
+	}
+
+	private handleError(error: Response) {
+		console.error(error);
+		return Observable.throw(error.json() || 'Server Error');
+	}
 
 }
