0dfcc31f2fe852ef06014227ed317dcea910dfd5
Bradford Taylor <bradford.taylor@gmail.com>
2017-07-26 13:23:06 -0500

    updated latest database changes and organized scheme to update ddls

--- src/main/resources/config/db-local/OPENLEARN_PUBLIC_ITEM_LINK.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam', 'https://cnn.com', 'Span', 'http://webmd.com');
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('luctus et ultrices posuere cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin', 'https://hp.com', 'Overhold', 'http://credera.com');
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante', 'https://wordpress.com', 'Duobam', 'http://ibm.com');
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('vivamus', 'https://reddit.com', 'Y-Solowarm', 'https://cnet.com');
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('A description of the item link', 'http://some.domain.com/information.pdf', 'Item Link Name', 'http://som.domain.com/image.jpg');
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('The description of another link', 'http://someother.domain.com/information.pdf', 'Another Link', 'http://someother.domain.com/image.jpg');
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('An item link description', 'http://some.domain.com/information.pdf', 'Item Link 1', 'http://some.domain.com/mage.jpg');
-INSERT INTO ITEM_LINK (DESCRIPTION, ITEM_URL, NAME, THUMBNAIL_IMAGE_URL) VALUES ('Another description for an item link', 'http://some.domain.com/moreinformation.pdf', 'Item Link 2', 'http://some.domain.com/image2.jpg');
--- src/main/resources/config/db-local/OPENLEARN_PUBLIC_SCHOOL.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO SCHOOL (DISTRICT, NAME, ADDRESS_ID) VALUES ('Denver', 'West High School', null);
-INSERT INTO SCHOOL (DISTRICT, NAME, ADDRESS_ID) VALUES ('JeffCo', 'An ELementary School', null);
-INSERT INTO SCHOOL (DISTRICT, NAME, ADDRESS_ID) VALUES ('Boulder', 'Some Nursery School', null);
