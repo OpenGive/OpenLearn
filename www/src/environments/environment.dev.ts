17c91e731d052f6ccabfe8ac6a736e85969fa53e
Bradford Taylor <bradford.taylor@gmail.com>
2017-07-27 21:41:45 -0500

    corrected backend url

--- www/src/environments/environment.dev.ts
+++ www/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   name: 'DEV', // named dev, but matches JHipster test
-  backend: 'https://dev.api.openlearn.opengive.io'
+  backend: 'https://dev-api.openlearn.opengive.io'
 };
