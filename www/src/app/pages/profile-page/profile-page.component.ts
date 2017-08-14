5930f8d3a3570cf47e93ba5ef6636f1314199298
Kevin Avila <kevin.avila@utexas.edu>
2017-08-14 12:53:35 -0500

    Nav bar now updates immediately when saving profile info.

--- www/src/app/pages/profile-page/profile-page.component.ts
+++ www/src/app/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from "@angular/core";
 import {UserService} from "../../services/user.service";
 import {AccountService} from "../../shared/auth/account.service";
+import {Principal} from "../../shared/auth/principal.service";
 
 @Component({
   selector: 'profile-form',
@@ -12,7 +13,7 @@ export class ProfilePageComponent {
   @Input() profile: any = {};
   @Input() editing: boolean;
 
-  constructor(private userService: UserService, private accountService: AccountService) {
+  constructor(private userService: UserService, private accountService: AccountService, private principal: Principal) {
     this.accountService.get().subscribe(resp => {this.profile = resp});
   }
 
@@ -26,7 +27,10 @@ export class ProfilePageComponent {
   }
 
   save(): void {
-    this.userService.update(this.profile).subscribe(resp => this.handleSaveResponse(resp));
+    this.userService.update(this.profile).subscribe(resp => {
+      this.handleSaveResponse(resp);
+      this.principal.authenticate(resp);
+    });
   }
 
   handleSaveResponse(resp): void {
