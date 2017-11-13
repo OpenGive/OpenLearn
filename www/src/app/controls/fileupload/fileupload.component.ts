ebbd0087f56d9c787a7b109260dcaf24dbb94639
Taylor Pickens <tapickens10@gmail.com>
2017-11-12 20:54:45 -0600

    -Styling file upload component

--- www/src/app/controls/fileupload/fileupload.component.ts
+++ www/src/app/controls/fileupload/fileupload.component.ts
@@ -8,7 +8,8 @@ const URL = '/api/courses/upload';
 
 @Component({
   selector: 'file-upload',
-  templateUrl: './fileupload.component.html'
+  templateUrl: './fileupload.component.html',
+  styleUrls: ['./fileupload.component.css']
 })
 export class FileUploadComponent {
 
@@ -16,9 +17,26 @@ export class FileUploadComponent {
     public hasBaseDropZoneOver:boolean = false;
     public hasAnotherDropZoneOver:boolean = false;
 
+    columns: any[];
+
     constructor(private _cookieService: CookieService) {
         let tokenObject = this._cookieService.getObject('token') as any;
         this.uploader.authToken = 'Bearer ' + tokenObject.access_token;
+
+        this.columns = [
+          {
+            id: "file.name",
+            name: "Name"
+          },
+          {
+            id: "file.date",
+            name: "Date"
+          },
+          {
+            id: "file.size",
+            name: "Size"
+          }
+        ];
     }
 
     public fileOverBase(e:any):void {
